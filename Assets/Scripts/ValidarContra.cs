74f42cfd04683ba22863f10b9d1e836ebdd79756
SantiagoMartin1 <101400604+SantiagoMartin1@users.noreply.github.com>
2022-04-23 13:17:16 -0300

    Tp 2 Terminado desde video 5 hasta Final

---
+++ Assets/Scripts/ValidarContra.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContra : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    string contraseñaUsuario;
+    public Text texto_Msj;
+    public Text ingresoUsuario;
+    public GameObject cartel_Msj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+        cartel_Msj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContra()
+    {
+
+        contraseñaUsuario = ingresoUsuario.text;
+        
+        if (contraseñaUsuario == contraseniaCorrecta)
+        {
+            cartel_Msj.SetActive(true);
+            texto_Msj.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+
+        }
+
+        else
+        {
+            cartel_Msj.SetActive(true);
+            texto_Msj.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+
+    }
+}
